3f0d2425cd8b89ac14235fb5fd36f7a852661613
Eugen <gram25042002@gmail.com>
2022-03-23 13:31:28 +0200

    add table migrations

---
+++ database/migrations/2022_03_23_112620_create_reservations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('book_id');
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('CASCADE');
+            $table->string('email');
+            $table->integer('quantity')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
