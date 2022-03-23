1c060dc5d4853eb28ea1aa37b95de93d62c17f1e
Eugen <gram25042002@gmail.com>
2022-03-23 18:45:53 +0200

    fix book update object

--- app/Http/Requests/Book/UpdateBookRequest.php
+++ app/Http/Requests/Book/UpdateBookRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests\Book;
 use App\Models\Book;
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateBookRequest extends FormRequest
+class UpdateBookRequest extends CreateBookRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,15 +24,6 @@ class UpdateBookRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-//            'title' => 'string|min:' , Book::MIN_TITLE_LENGTH . '|max:' . Book::MAX_TITLE_LENGTH,
-//            'short_description' => 'string|min:' . Book::MIN_DESCRIPTION_LENGTH . '|max:' . Book::MIN_DESCRIPTION_LENGTH,
-//            'amount' => 'numeric|min:' . Book::MIN_AMOUNT . '|max:' . Book::MAX_AMOUNT,
-//            'author_id' => 'integer|exists:authors,id',
-//            'title' => 'required|string|min:1|max:64',
-//            'short_description' => 'required|string|min:1|max:200',
-//            'amount' => 'required|numeric|min:0|max:1000',
-//            'author_id' => 'integer|exists:authors,id',
-        ];
+        return parent::rules();
     }
 }
