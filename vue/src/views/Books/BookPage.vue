b1f1902de91138b54c2d74cbb2121d015344f949
Eugen <gram25042002@gmail.com>
2022-03-24 14:34:45 +0200

    add book reservation

--- vue/src/views/Books/BookPage.vue
+++ vue/src/views/Books/BookPage.vue
@@ -1,4 +1,5 @@
 <template>
+    <div v-show="success">Book was successfully reserved</div>
     <p>{{ bookData.title }}</p>
     <p>{{ bookData.short_description }}</p>
     Remain: <span>{{ bookData.amount }}</span><br>
@@ -8,6 +9,20 @@
             {{ bookData.author.first_name }} {{ bookData.author.last_name }}
         </a>
     </div>
+    <div v-if="bookData.amount">
+        <form @submit.prevent="reserve">
+        How many books do you want to reserve? <br>
+        <input type="number" v-model="fields.quantity" min="1"><br>
+        Your email: <br>
+        <input type="email" v-model="fields.email"><br>
+        <button type="submit">
+            Reserve book
+        </button>
+        </form>
+    </div>
+    <div v-else>
+        There are now available books to reserve. Visit this page later
+    </div>
 </template>
 
 <script>
@@ -16,7 +31,9 @@ export default {
     name: "BookPage",
     data() {
         return {
-            bookData: {}
+            bookData: {},
+            fields: {},
+            success: false,
         }
     },
     created() {
@@ -33,6 +50,18 @@ export default {
                 {
                     authorId: authorId,
                 })
+        },
+        reserve(){
+            this.fields.book_id = this.bookData.id;
+            axios.post('http://localhost/api/reservations/reserve', this.fields).
+                then((response) => {
+                    this.fields = {};
+                    this.success = true;
+                    setTimeout(() => {
+                        location.reload()
+                    }, 3000)
+            }).
+                catch((error) => console.log(error))
         }
     }
 }
