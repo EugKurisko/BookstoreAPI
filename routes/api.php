420d0d4ff5791fa8d760c212e7d0d26540e1c461
Eugen <gram25042002@gmail.com>
2022-03-24 17:19:00 +0200

    remove namespace

--- routes/api.php
+++ routes/api.php
@@ -21,26 +21,24 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['namespace' => 'API'], function () {
-    Route::group(['prefix' => 'authors'], function () {
-        Route::get('/all', [AuthorController::class, 'allAuthors']);
-        Route::get('/allWithBooks', [AuthorController::class, 'allAuthorsWithBooks']);
-        Route::post('/create', [AuthorController::class, 'createAuthor']);
-        Route::post('{author}/update', [AuthorController::class, 'updateAuthor']);
-        Route::get('{author}/show', [AuthorController::class, 'showAuthor']);
-        Route::post('{author}/delete', [AuthorController::class, 'deleteAuthor']);
-    });
+Route::group(['prefix' => 'authors'], function () {
+    Route::get('/all', [AuthorController::class, 'allAuthors']);
+    Route::get('/allWithBooks', [AuthorController::class, 'allAuthorsWithBooks']);
+    Route::post('/create', [AuthorController::class, 'createAuthor']);
+    Route::post('{author}/update', [AuthorController::class, 'updateAuthor']);
+    Route::get('{author}/show', [AuthorController::class, 'showAuthor']);
+    Route::post('{author}/delete', [AuthorController::class, 'deleteAuthor']);
+});
 
-    Route::group(['prefix' => 'books'], function () {
-       Route::get('/all', [BookController::class, 'allBooks']);
-       Route::get('/allWithAuthors', [BookController::class, 'allBooksWithAuthors']);
-       Route::post('/create', [BookController::class, 'createBook']);
-       Route::get('{book}/show', [BookController::class, 'showBook']);
-       Route::post('{book}/update', [BookController::class, 'updateBook']);
-       Route::post('{book}/delete', [BookController::class, 'deleteBook']);
-    });
+Route::group(['prefix' => 'books'], function () {
+    Route::get('/all', [BookController::class, 'allBooks']);
+    Route::get('/allWithAuthors', [BookController::class, 'allBooksWithAuthors']);
+    Route::post('/create', [BookController::class, 'createBook']);
+    Route::get('{book}/show', [BookController::class, 'showBook']);
+    Route::post('{book}/update', [BookController::class, 'updateBook']);
+    Route::post('{book}/delete', [BookController::class, 'deleteBook']);
+});
 
-    Route::group(['prefix' => 'reservations'], function () {
-       Route::post('/reserve', [ReservationController::class, 'reserveBook']);
-    });
+Route::group(['prefix' => 'reservations'], function () {
+    Route::post('/reserve', [ReservationController::class, 'reserveBook']);
 });
