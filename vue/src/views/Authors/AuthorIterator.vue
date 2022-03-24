e31c4897731b4451356d55778adf17d10fab07f1
Eugen <gram25042002@gmail.com>
2022-03-24 12:23:10 +0200

    add show author method

--- vue/src/views/Authors/AuthorIterator.vue
+++ vue/src/views/Authors/AuthorIterator.vue
@@ -1,16 +1,28 @@
 <template>
     <div v-for="author in authors" :key="author.id">
-        <p>
+        <a @click="showAuthor(author.id)">
             {{ author.first_name }} {{ author.last_name }}
-        </p>
+        </a> <br>
     </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
     name: "AuthorIterator",
     props: {
         authors: Array
+    },
+    methods: {
+        showAuthor(authorId) {
+            axios.get(`http://localhost/api/authors/${authorId}/show`).
+                then((response) => {
+                    console.log(response.data)
+            }).
+                catch((error) => {
+                    console.log(error)
+            })
+        }
     }
 }
 </script>
